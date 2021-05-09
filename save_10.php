6c049f0121038e257bd1d7c12715597f19b29bd6
VovaD <56256864+VovaD199@users.noreply.github.com>
2021-05-10 02:05:49 +0300

    task_10
    task_10

---
+++ save_10.php
@@ -0,0 +1,30 @@
+<?php
+session_start();
+
+$text = $_POST['text'];
+
+$pdo = new PDO("mysql:host=localhost;dbname=mdev;", "root", "");
+
+$sql = "SELECT * FROM my_table WHERE text=:text";
+$statement = $pdo->prepare($sql);
+$statement->execute(['text' => $text]);
+$task = $statement->fetch(PDO::FETCH_ASSOC);
+
+if(!empty($task)){
+  $message = "Введенная запись уже присутствует в таблице.";
+  $_SESSION['danger'] = $message;
+
+  header("Location: /task_10.php");
+  exit;
+}
+
+$sql = "INSERT INTO my_table (text) VALUES (:text)";
+$statement = $pdo->prepare($sql);
+$statement->execute(['text' => $text]);
+
+$message = "Введенная запись добавленна в таблицу.";
+$_SESSION['success'] = $message;
+
+header("Location: /task_10.php");
+
+?>
